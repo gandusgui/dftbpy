34b16e8c2d0d7930586c416ffd0845a537b46a50
ggandus <gandusgui@gmail.com>
2023-08-30 16:28:10 +0200

    enh: typing setups

--- src/dftbpy/setups.py
+++ src/dftbpy/setups.py
@@ -4,134 +4,9 @@ from ase.neighborlist import first_neighbors, primitive_neighbor_list
 from ase.units import Bohr
 
 from dftbpy.param import SlaterKosterTable
+from dftbpy.param.atom import Atom
 from dftbpy.param.configs import angular_number
 from dftbpy.slako import SlaterKosterParam
-
-
-def setupproperty(name, doc):
-    """Helper function to easily create Setup attribute property."""
-
-    def getter(self):
-        return self.get(name)
-
-    def setter(self, value):
-        self.set(name, value)
-
-    def deleter(self):
-        self.delete(name)
-
-    return property(getter, setter, deleter, doc)
-
-
-class Setup:
-    """Setup for an atom or element."""
-
-    def __init__(self, /, atom=None, index=None, setups=None) -> None:
-        self.data = d = {}
-
-        if setups is None:
-            eners = []  # energies
-            d["symbol"] = atom.symbol
-            d["U"] = getattr(atom, "U", 0.0)  # backward compatibility
-            no = 0
-            nel = 0
-            for nlf, e in atom.valence_configuration.items():
-                no_ = 2 * angular_number[nlf[1]] + 1
-                no += no_
-                nel += int(nlf[2:])
-                eners.extend(no_ * [e])
-            d["no"] = no
-            d["nel"] = nel
-            d["energies"] = np.array(eners)
-
-        self.index = index
-        self.setups = setups
-
-    def get(self, name):
-        """Get name attribute."""
-        if self.setups is None:
-            return self.data[name]
-
-        setups = self.setups
-        symbol = setups.symbols[self.index]
-
-        if name == "orbitals_slice":
-            fo = setups.first_orbitals
-            return slice(fo[self.index], fo[self.index + 1])
-
-        if name == "neighbors":
-            return (
-                (setups[nn], rho, dist)
-                for nn, rho, dist in zip(*setups.get_neighbors(self.index))
-            )
-
-        return setups.elements[symbol].data[name]
-
-    def set(self, name, value):
-        """Set name attribute."""
-        if self.setups is None:
-            self.data[name] = value
-
-    symbol = setupproperty("symbol", "Chemical symbol")
-    # configuration = setupproperty("configuration", "Valence state quantum numbers")
-    no = setupproperty("no", "Number of orbitals")
-    nel = setupproperty("nel", "Number of electrons")
-    U = setupproperty("U", "Hubbard parameter")
-    energies = setupproperty("energies", "Orbital energies")
-    orbitals_slice = setupproperty("orbitals_slice", "Orbital indices")
-    neighbors = setupproperty("neighbors", "Neighbors")
-
-
-class Setups:
-    """Setups for a system of atoms."""
-
-    def __init__(self, atoms: Atoms, slakos: dict[SlaterKosterTable]) -> None:
-        self.symbols = atoms.symbols
-        self.atoms = atoms
-
-        self.slakos = {}  # Slater-Koster pairs
-        self.elements = {}  # Unique elements
-        self.cutoffs = {}  # Cutoff distances pairs
-        for (s1, s2), skt in slakos.items():  # skt = SlaterKosterTable
-            self.slakos[s1, s2] = slako12 = SlaterKosterParam(skt, (s1, s2))
-            self.slakos[s2, s1] = slako21 = SlaterKosterParam(skt, (s2, s1))
-
-            for symbol in (s1, s2):
-                if symbol not in self.elements:
-                    self.elements[symbol] = Setup(atom=skt.atoms[symbol])
-
-            self.cutoffs[s1, s2] = slako12.cutoff
-            self.cutoffs[s2, s1] = slako21.cutoff
-
-        na = len(self.atoms)
-        self.first_orbitals = np.zeros(na + 1, int)  # first orbital of each atom
-        self.nel = 0  # total number of electrons
-        self.no = 0  # total number of orbitals
-        for index, s in enumerate(self.symbols):
-            self.nel += self.elements[s].nel
-            self.no += self.elements[s].no
-            self.first_orbitals[index + 1] = self.no
-
-        self.nl = NeighborList(cutoffs=self.cutoffs)
-        self.nl.update(self.atoms)
-
-    def get_total_number_of_orbitals(self):
-        return self.no
-
-    def get_total_number_of_electrons(self):
-        return self.nel
-
-    def get_neighbors(self, a):
-        return self.nl.get_neighbors(a)
-
-    def __len__(self):
-        return len(self.atoms)
-
-    def __getitem__(self, a) -> Setup:
-        return Setup(index=a, setups=self)
-
-    def __iter__(self) -> iter:
-        yield from (self[i] for i in range(len(self)))
 
 
 class NeighborList:
@@ -140,10 +15,8 @@ class NeighborList:
         self.bothways = False
         self.skin = skin  # Any change in positions dp | dp < skin^2 triggers update
         self.self_interaction = False
-        self.sorted = False
+        self.sorted = True
         self.nupdates = 0
-        self.nneighbors = 0
-        self.npbcneighbors = 0
 
     def update(self, atoms):
         """Make sure the list is up to date."""
@@ -209,7 +82,7 @@ class NeighborList:
         self.i = i
         self.j = j
         self.d = d
-        self.rho = D / d[:, None]  # self_interaction=False, no D==0.
+        self.rho = D / d[:, None]  # self_interaction=False
         self.D = D
 
         self.first_neigh = first_neighbors(len(positions), i)
@@ -231,3 +104,136 @@ class NeighborList:
             self.rho[self.first_neigh[a] : self.first_neigh[a + 1]],
             self.d[self.first_neigh[a] : self.first_neigh[a + 1]],
         )
+
+
+class Setups:
+    """Setups for a system of atoms."""
+
+    def __init__(self, atoms: Atoms, slakos: dict[SlaterKosterTable]) -> None:
+        self.symbols = atoms.symbols
+        self.atoms = atoms
+
+        self.slakos = {}  # Slater-Koster pairs
+        self.elements = {}  # Unique elements
+        self.cutoffs = {}  # Cutoff distances pairs
+        for (s1, s2), skt in slakos.items():  # skt = SlaterKosterTable
+            self.slakos[s1, s2] = slako12 = SlaterKosterParam(skt, (s1, s2))
+            self.slakos[s2, s1] = slako21 = SlaterKosterParam(skt, (s2, s1))
+
+            for symbol in (s1, s2):
+                if symbol not in self.elements:
+                    self.elements[symbol] = Setup(atom=skt.atoms[symbol])
+
+            self.cutoffs[s1, s2] = slako12.cutoff
+            self.cutoffs[s2, s1] = slako21.cutoff
+
+        self.orbitals_slices = []
+        self.nel = 0  # total number of electrons
+        self.no = 0  # total number of orbitals
+        for s in self.symbols:
+            el = self.elements[s]
+            self.orbitals_slices.append(slice(self.no, self.no + el.no))
+            self.nel += el.nel
+            self.no += el.no
+
+        self.nl = NeighborList(cutoffs=self.cutoffs)
+
+    @property
+    def nupdates(self):
+        return self.nl.nupdates
+
+    def update(self, atoms):
+        """Update neighbor list."""
+        return self.nl.update(atoms)
+
+    def get_total_number_of_orbitals(self):
+        return self.no
+
+    def get_total_number_of_electrons(self):
+        return self.nel
+
+    def get_neighbors(self, a):
+        return self.nl.get_neighbors(a)
+
+    def __len__(self):
+        return len(self.atoms)
+
+    def __getitem__(self, a):
+        return Setup(index=a, setups=self)
+
+    def __iter__(self) -> iter:
+        yield from (self[i] for i in range(len(self)))
+
+
+def setupproperty(name, doc):
+    """Helper function to easily create Setup attribute property."""
+
+    def getter(self):
+        return self.get(name)
+
+    def setter(self, value):
+        self.set(name, value)
+
+    def deleter(self):
+        self.delete(name)
+
+    return property(getter, setter, deleter, doc)
+
+
+class Setup:
+    """Setup for an atom or element."""
+
+    def __init__(
+        self, /, atom: Atom = None, index: int = None, setups: Setups = None
+    ) -> None:
+        self.data = d = {}
+
+        if setups is None:
+            eners = []  # energies
+            d["symbol"] = atom.symbol
+            d["U"] = getattr(atom, "U", 0.0)  # backward compatibility
+            no = 0
+            nel = 0
+            for nlf, e in atom.valence_configuration.items():
+                no_ = 2 * angular_number[nlf[1]] + 1
+                no += no_
+                nel += int(nlf[2:])
+                eners.extend(no_ * [e])
+            d["no"] = no
+            d["nel"] = nel
+            d["energies"] = np.array(eners)
+
+        self.index = index
+        self.setups = setups
+
+    def get(self, name):
+        """Get name attribute."""
+        if self.setups is None:
+            return self.data[name]
+
+        setups = self.setups
+        symbol = setups.symbols[self.index]
+
+        if name == "orbitals_slice":
+            return setups.orbitals_slices[self.index]
+
+        if name == "neighbors":
+            return (
+                (setups[nn], rho, dist)
+                for nn, rho, dist in zip(*setups.get_neighbors(self.index))
+            )
+
+        return setups.elements[symbol].data[name]
+
+    def set(self, name, value):
+        """Set name attribute."""
+        if self.setups is None:
+            self.data[name] = value
+
+    symbol = setupproperty("symbol", "Chemical symbol")
+    no = setupproperty("no", "Number of orbitals")
+    nel = setupproperty("nel", "Number of electrons")
+    U = setupproperty("U", "Hubbard parameter")
+    energies = setupproperty("energies", "Orbital energies")
+    orbitals_slice = setupproperty("orbitals_slice", "Orbital indices")
+    neighbors = setupproperty("neighbors", "Neighbors")
